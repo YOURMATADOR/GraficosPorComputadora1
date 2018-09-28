2b87e4b0eeaa15bc810ca4b290838146aa3cfdfb
Tu matador <jesus.amancilla@alumnos.udg.mx>
2018-09-28 09:08:57 -0500

    tarea letra

--- unidad2/js/nombre.js
+++ unidad2/js/nombre.js
@@ -1,38 +1,44 @@
 let canvas = document.getElementById("miCanvas");
 let contexto = canvas.getContext("2d");
-var color = "#ffffff";
+var color = "#000000";
 const pintarPixel = (context, x, y, color = "#000000") => {
   context.fillStyle = color;
   context.fillRect(x, y, 1, 1);
 };
-const nombre = [
-  { x: 10, y: 90, x2: 10, y2: 300 },
-  { x: 10, y: 90, x2: 180, y2: 90 },
-  { x: 10, y: 180, x2: 180, y2: 180 },
-  { x: 10, y: 300, x2: 180, y2: 300 }
-];
-let puntos = [];
-function dibujar() {
-  //Esta funci�n se ejecuta cada que se hace clic sobre el lienz
 
-  // if(primerPunto){  //Si es el primer clic, se lee el primer punto de la l�nea
-  //   puntos.push({x:nombre[0].x, y:nombre[0].y});
-  //   ponerPixel(contexto, puntos[puntos.length-1].x, puntos[puntos.length-1].y, color);
-  //   primerPunto = false;
-  // }
-  //pintar l�nea
+const nombre = [
+  { x: 60, y: 100 },
+  { x: 60, y: 300 },
+  { x: 60, y: 100 },
+  { x: 150, y: 100 },
+  { x: 60, y: 200 },
+  { x: 150, y: 200 },
+  { x: 60, y: 300 },
+  { x: 150, y: 300 }
+];
+
+var puntos = [];
+
+function dibujar() {
+  puntos = [];
+  contexto.clearRect(0, 0, canvas.width, canvas.height);
+
   nombre.map((v, i) => {
+    if (i <= 0) {
+      puntos.push({ x: nombre[0].x, y: nombre[0].y });
+    }
+
     lineaBresenham(
+      puntos[puntos.length - 1].x,
+      puntos[puntos.length - 1].y,
       nombre[i].x,
       nombre[i].y,
-      nombre[i].x2,
-      nombre[i].y2,
       contexto,
       color
     );
-    puntos.push({ x: nombre[i].x, y: nombre[i].y});
-  });
 
+    puntos.push({ x: nombre[i].x, y: nombre[i].y });
+  });
 }
 
 //Implementaci�n del algoritmo de Bresenham para l�neas
@@ -59,81 +65,99 @@ function lineaBresenham(x0, y0, x1, y1, contexto, color) {
   }
 }
 
-function escalar(){   
-  console.log('Escalar');
-  var sx = parseInt(document.getElementById("sx").value); //obtenemos el valor 
-  var sy = parseInt(document.getElementById("sy").value); //obtenemos el valor 
+function escalar() {
+  var sx = parseInt(document.getElementById("sx").value); //obtenemos el valor
+  var sy = parseInt(document.getElementById("sy").value); //obtenemos el valor
 
-  var T= new Array (); 
-      T[0] = new Array (sx, 0,  0); 
-      T[1] = new Array (0,  sy, 0); 
-      T[2] = new Array (0,  0,  1); 
-      
-      transformar2D(T);
+  var T = new Array();
+  T[0] = new Array(sx, 0, 0);
+  T[1] = new Array(0, sy, 0);
+  T[2] = new Array(0, 0, 1);
+
+  transformar2D(T);
 }
 
-function trasladar(){   
-  var dx = parseInt(document.getElementById("dx").value); //obtenemos el valor 
-  var dy = parseInt(document.getElementById("dy").value); //obtenemos el valor 
+// function trasladar(){
+//   var dx = parseInt(document.getElementById("dx").value); //obtenemos el valor
+//   var dy = parseInt(document.getElementById("dy").value); //obtenemos el valor
 
-  var T= new Array (); 
-      T[0] = new Array (1, 0, dx); 
-      T[1] = new Array (0, 1, dy); 
-      T[2] = new Array (0, 0, 1); 
-      
-      transformar2D(T);
+//   var T= new Array ();
+//       T[0] = new Array (1, 0, dx);
+//       T[1] = new Array (0, 1, dy);
+//       T[2] = new Array (0, 0, 1);
+
+//       transformar2D(T);
+// }
+
+function mover(x, y) {
+  var T = new Array();
+  T[0] = new Array(1, 0, x);
+  T[1] = new Array(0, 1, y);
+  T[2] = new Array(0, 0, 1);
+
+  transformar2D(T);
 }
 
-function mover(x, y){   
-
-  var T= new Array (); 
-      T[0] = new Array (1, 0, x); 
-      T[1] = new Array (0, 1, y); 
-      T[2] = new Array (0, 0, 1); 
-      
-      transformar2D(T);
-}
-
-function rotar(){   
+function rotar() {
   var a = parseInt(document.getElementById("angulo").value); //obtenemos el valor del �ngulo de rotaci�n
 
-  var x = puntos[0].x;  //guardamos ubicaci�n origial
+  var x = puntos[0].x; //guardamos ubicaci�n origial
   var y = puntos[0].y;
 
-  mover(-x,-y); //trasladamos al origen
+  mover(-x, -y); //trasladamos al origen
 
-  var T= new Array (); 
-      T[0] = new Array (Math.cos(a), -Math.sin(a), 0); 
-      T[1] = new Array (Math.sin(a), Math.cos(a), 0); 
-      T[2] = new Array (0, 0, 1); 
-      
-      transformar2D(T);  //rotamos
+  var T = new Array();
+  T[0] = new Array(Math.cos(a), -Math.sin(a), 0);
+  T[1] = new Array(Math.sin(a), Math.cos(a), 0);
+  T[2] = new Array(0, 0, 1);
 
-      mover(x,y);  //trasladamos a posici�n inicial
+  transformar2D(T); //rotamos
+
+  mover(x, y); //trasladamos a posici�n inicial
 }
 
-function transformar2D(m){
-  for(var i = 0; i < puntos.length; i++) {
-     
+function transformar2D(m) {
+  console.log("Transformar");
+  for (var i = 0; i < puntos.length; i++) {
     var p = new Array(puntos[i].x, puntos[i].y, 1);
 
-    puntos[i].x =  Math.round((m[0][0]*p[0]) + (m[0][1]*p[1]) + (m[0][2]*p[2]));
-    puntos[i].y =  Math.round((m[1][0]*p[0]) + (m[1][1]*p[1]) + (m[1][2]*p[2])); 
-             
+    puntos[i].x = Math.round(m[0][0] * p[0] + m[0][1] * p[1] + m[0][2] * p[2]);
+    puntos[i].y = Math.round(m[1][0] * p[0] + m[1][1] * p[1] + m[1][2] * p[2]);
   }
   repintar();
 }
 
-
-function repintar(){
+function repintar() {
   contexto.clearRect(0, 0, canvas.width, canvas.height);
-  for(var i = 1; i < puntos.length; i++) {
-    lineaBresenham(puntos[i-1].x, puntos[i-1].y, puntos[i].x, puntos[i].y, contexto, color);
+  for (var i = 1; i < puntos.length; i++) {
+    lineaBresenham(
+      puntos[i - 1].x,
+      puntos[i - 1].y,
+      puntos[i].x,
+      puntos[i].y,
+      contexto,
+      color
+    );
   }
-  
 }
 
-document.getElementById('sx').onchange = escalar;
-document.getElementById('sy').onchange = escalar;
-
-dibujar();
+function cambioColor(e) {
+  console.log(e.target);
+  color = e.target.value;
+  repintar();
+}
+document.getElementById("c").onchange = cambioColor;
+document.getElementById("sx").onchange = escalar;
+document.getElementById("sy").onchange = escalar;
+document.getElementById("mx").onchange = () =>
+  mover(
+    document.getElementById("mx").value,
+    document.getElementById("my").value
+  );
+document.getElementById("my").onchange = () =>
+  mover(
+    document.getElementById("mx").value,
+    document.getElementById("my").value
+  );
+document.getElementById("angulo").onchange = rotar;
+canvas.onclick = dibujar;
